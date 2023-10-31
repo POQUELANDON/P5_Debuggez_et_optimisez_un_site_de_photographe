0f812ac5376128e9c5a17a1287ba67f99ff381ed
Natacha POQUE-LANDON <natacha.poque@hotmail.fr>
2023-10-31 10:05:37 +0100

    Minifaction script.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,17 +1,1 @@
-// Utilisation de mauGallery
-document.addEventListener('DOMContentLoaded', function () {
-    const gallery = document.querySelectorAll('.gallery');
-    mauGallery(gallery, {
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
-});
+document.addEventListener("DOMContentLoaded", function () { let o = document.querySelectorAll(".gallery"); mauGallery(o, { columns: { xs: 1, sm: 2, md: 3, lg: 3, xl: 3 }, lightBox: !0, lightboxId: "myAwesomeLightbox", showTags: !0, tagsPosition: "top" }) });
