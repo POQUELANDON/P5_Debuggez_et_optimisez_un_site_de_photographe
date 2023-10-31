6cb959a0bea14585766a52f865ebe8f3009c8a98
Natacha POQUE-LANDON <natacha.poque@hotmail.fr>
2023-10-31 10:04:28 +0100

    Suppression bibliothèque jQuery, modification maugalley et script .js en javascript natif ; minifaction du js.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,18 +1,17 @@
+// Utilisation de mauGallery
 document.addEventListener('DOMContentLoaded', function () {
-    var gallery = document.querySelector('.gallery');
-    if (gallery) {
-        mauGallery({
-            columns: {
-                xs: 1,
-                sm: 2,
-                md: 3,
-                lg: 3,
-                xl: 3
-            },
-            lightBox: true,
-            lightboxId: 'myAwesomeLightbox',
-            showTags: true,
-            tagsPosition: 'top'
-        });
-    }
+    const gallery = document.querySelectorAll('.gallery');
+    mauGallery(gallery, {
+        columns: {
+            xs: 1,
+            sm: 2,
+            md: 3,
+            lg: 3,
+            xl: 3
+        },
+        lightBox: true,
+        lightboxId: 'myAwesomeLightbox',
+        showTags: true,
+        tagsPosition: 'top'
+    });
 });
